e11234e01503bfb644aa13bd1e3e1d19387744e4
alecarpy24 <aleannyp@gmail.com>
2024-10-16 14:10:43 +0200

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/alecarpy24/proyectos-fct/tree/main/react-rick-and-morty",
+  base: "https://github.com/alecarpy24/react-rick-and-morty",
   plugins: [react()],
 })
