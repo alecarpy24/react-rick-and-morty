57a0de693f5f9ce8fa2a897ab152c877096b51a9
alecarpy24 <aleannyp@gmail.com>
2024-10-16 13:58:54 +0200

    Rick and Morty

---
+++ src/components/Character.jsx
@@ -0,0 +1,13 @@
+
+
+function Character({character}) {
+  return (
+    <div className="text-center p-5">
+            <h3>{character.name}</h3>
+            <img className="img-fluid rounded-circle" src={character.image} alt={character.name}/>
+            <p>{character.origin.name}</p>
+            </div>
+  )
+}
+
+export {Character}
